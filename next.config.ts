5eb7aba48899b2ac3cef16dcbee22dc4db435b5a
Thievn <deanoneill69@gmail.com>
2026-08-21 07:19:48 -0400

    Allow RAWG media host for cover images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "media.rawg.io" },
+      { protocol: "https", hostname: "**.rawg.io" },
+    ],
+  },
 };
 
 export default nextConfig;

1c6e5ae6ba4aa0e6f8cbaaefb927c16f4e168bb2
Thievn <deanoneill69@gmail.com>
2026-08-21 07:20:10 -0400

    Fix RAWG remotePatterns hostname syntax

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "media.rawg.io" },
-      { protocol: "https", hostname: "**.rawg.io" },
+      { protocol: "https", hostname: "api.rawg.io" },
     ],
   },
 };
